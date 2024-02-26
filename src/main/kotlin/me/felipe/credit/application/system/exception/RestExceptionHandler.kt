c376013efc81685a43a8d20efaab1983666b464e
Felipe Barbosa <rafaelt.ibarbosa@gmail.com>
2024-02-26 16:43:00 -0300

    Confict Exception

--- src/main/kotlin/me/felipe/credit/application/system/exception/RestExceptionHandler.kt
+++ src/main/kotlin/me/felipe/credit/application/system/exception/RestExceptionHandler.kt
@@ -1,5 +1,7 @@
 package me.felipe.credit.application.system.exception
 
+import org.hibernate.exception.DataException
+import org.springframework.dao.DataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
@@ -16,20 +18,33 @@ class RestExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handlerValidException(ex: MethodArgumentNotValidException): ResponseEntity<ExceptionDetails> {
         val erros: MutableMap<String, String?> = HashMap()
-        ex.bindingResult.allErrors.stream().forEach {
-            erro: ObjectError ->
+        ex.bindingResult.allErrors.stream().forEach { erro: ObjectError ->
             val fieldName: String = (erro as FieldError).field
             val messageError: String? = erro.defaultMessage
             erros[fieldName] = messageError
         }
 
         return ResponseEntity(
-            ExceptionDetails(title = "Bad Request! Consult the documentation",
+            ExceptionDetails(
+                title = "Bad Request! Consult the documentation",
                 timestamp = LocalDateTime.now(),
                 status = HttpStatus.BAD_REQUEST.value(),
                 exception = ex.javaClass.toString(),
                 details = erros
-                ), HttpStatus.BAD_REQUEST
+            ), HttpStatus.BAD_REQUEST
+        )
+    }
+
+    @ExceptionHandler(DataAccessException::class)
+    fun handlerValidException(ex: DataAccessException): ResponseEntity<ExceptionDetails> {
+        return ResponseEntity(
+            ExceptionDetails(
+                title = "Conflict! Consult the documentation",
+                timestamp = LocalDateTime.now(),
+                status = HttpStatus.BAD_REQUEST.value(),
+                exception = ex.javaClass.toString(),
+                details = mutableMapOf(ex.cause.toString() to ex.message)
+            ), HttpStatus.CONFLICT
         )
     }
 }
