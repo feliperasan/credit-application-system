073edfbec84bdf90b421a60a211112e7f35df5cc
Felipe Barbosa <rafaelt.ibarbosa@gmail.com>
2024-02-26 00:12:55 -0300

    Implementação - [dto]CustomerDto e [controller]CustomerResourse

---
+++ src/main/kotlin/me/felipe/credit/application/system/controller/CustomerResource.kt
@@ -0,0 +1,21 @@
+package me.felipe.credit.application.system.controller
+
+import me.felipe.credit.application.system.dto.CustomerDto
+import me.felipe.credit.application.system.service.impl.CustomerService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/customer")
+class CustomerResource(
+    private val customerService: CustomerService
+){
+
+    @PostMapping
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
+        val savedCustomer = this.customerService.save(customerDto.toEntity())
+        return "Customer ${savedCustomer.email} saved!"
+    }
+}
