0ce4a490b232aea13402888cdc266fb5eee881c9
Felipe Barbosa <rafaelt.ibarbosa@gmail.com>
2024-02-25 23:14:48 -0300

    Implementação - Algumas funções ICreditService e Regras de Negócio CreditService

---
+++ src/main/kotlin/me/felipe/credit/application/system/service/impl/CreditService.kt
@@ -0,0 +1,31 @@
+package me.felipe.credit.application.system.service.impl
+
+import me.felipe.credit.application.system.entity.Credit
+import me.felipe.credit.application.system.repository.CreditRepository
+import me.felipe.credit.application.system.service.ICreditService
+import java.util.*
+
+class CreditService(
+    private var creditRepository: CreditRepository,
+    private val customerService: CustomerService
+) : ICreditService {
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> {
+        return this.creditRepository.findAllByCustomerId(customerId)
+    }
+
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit: Credit = this.creditRepository.findByCreditCode(creditCode)
+            ?: throw RuntimeException("Creditcode $creditCode not found")
+        return if (credit.customer?.id == customerId)
+            credit
+        else
+            throw RuntimeException("Contact admin")
+    }
+}
